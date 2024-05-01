cb0a1b32c1804573a2695fbaed10f72287f850a7
Ahmed Alghali <95593096+A7med7x7@users.noreply.github.com>
2024-05-01 10:08:06 +0300

    gihub added

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ for member in team_members:
 
 content += """
 has poured their expertise and passion into creating this platform for your benefit. We invite you to explore, learn, and have fun!
-**Explore the Code on GitHub:** [Tesnor-Flow SC Fertilizer and Crop Recommendation Recomm](https://github.com/A7med7x7/Tesnor-Flow-SC-Fertilizer-and-Crop-Recommendation-Recomm)
+**Explore the Code on GitHub:** [GitHub Repository](https://github.com/A7med7x7/Tesnor-Flow-SC-Fertilizer-and-Crop-Recommendation-Recomm)
 
 """
 
